abde21edaee621645320cbbab52bfa943cd9deb6
Renzo Ramos <rramos6.seidor@gmail.com>
2026-04-28 10:13:06 +0200

    Fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+
+  base: '/portafolio-69a3ed/',
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
   },
+
   server: {
     port: 5173,
     open: true,
     host: true,
   },
+
   publicDir: 'public',
 });
